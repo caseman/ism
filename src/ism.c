96fd9bdf96855aebf068118af12ebb1a960d5676
Casey Duncan <casey.duncan@coinbase.com>
2020-08-30 13:08:09 -0600

    working bearlibterminal app on macos

--- src/ism.c
+++ src/ism.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <ism.h>
-#include "fastrng.h"
-#include "perlin.h"
+#include "BearLibTerminal.h"
 
-int main(int argc, char *argv[]) {
-    uint32_t seed = strtoumax(argv[1], NULL, 10);
-    rand_seed(seed);
-    noise_ptable ptable = create_noise_ptable(rand_int32);
-    printf("Ism %d.%d %d\n", ISM_VERSION_MAJOR, ISM_VERSION_MINOR, seed);
-    for (float i = 0.0f; i < 5.0f; i += 1.0f) {
-        printf("height %.5f\n", noise1(ptable, i * .137f + 0.072));
+int main() {
+    terminal_open();
+    terminal_setf(
+        "window: size=80x25, cellsize=auto, title='Ism %d.%d';"
+        "font: default;"
+        "input: filter={keyboard}",
+        ISM_VERSION_MAJOR, ISM_VERSION_MINOR
+    );
+    terminal_color(0xFFFFFFFF);
+
+    terminal_clear();
+    terminal_printf(2, 23, "[color=orange]ESC.[/color] Exit");
+    terminal_refresh();
+
+    while (1) {
+
+        int key = terminal_read();
+        if (key == TK_ESCAPE || key == TK_CLOSE) {
+            break;
+        }
     }
+    terminal_close();
     return 0;
 }
